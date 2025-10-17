1c97c4077c90f94b9cbf073108aaa1917714281f
gotr00t0day <48636787+gotr00t0day@users.noreply.github.com>
2025-10-17 11:24:57 -0400

    Update executils.h

--- modules/executils.h
+++ modules/executils.h
@@ -2,6 +2,4 @@
 #include <string>
 #include <vector>
 
-int commandExists(const std::string& cmd);
 std::string execCommand(const std::string& cmd);
-void runCommand(const std::vector<const char*>& args);
